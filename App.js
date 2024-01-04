2b08c4e78691e7a8bfd97249390486da09da1da1
Ashutosh Shelke <ashutoshshelke07@gmail.com>
2024-01-04 16:55:49 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,17 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement(
+      "h1",
+      { id: "heading" },
+      "Hello world from React!"
+    )
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
