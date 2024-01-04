be97ba10e1641a3e5b3bce62ba63c0f195d33701
Ashutosh Shelke <ashutoshshelke07@gmail.com>
2024-01-05 04:27:46 +0530

    React components

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement(
-      "h1",
-      { id: "heading" },
-      "Hello world from React!"
-    )
-  )
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement(
+//       "h1",
+//       { id: "heading" },
+//       "Hello world from Ashutosh React!"
+//     )
+//   )
+// );
 
-root.render(parent)
+const Title = () => <h1 id="heading">Namaste React using JSX</h1>
+
+const Heading = () => <div>
+  <Title />
+  <h1 className="head">Hello from React component!</h1>
+</div>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Heading />);
